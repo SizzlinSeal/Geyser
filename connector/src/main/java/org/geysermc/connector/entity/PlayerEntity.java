ef042b0b655e7acf44aff07c5c9fb9214c8f04b0
Zach <hellohi3654@gmail.com>
2020-08-12 21:20:38 +1000

    Update PlayerEntity.java

--- connector/src/main/java/org/geysermc/connector/entity/PlayerEntity.java
+++ connector/src/main/java/org/geysermc/connector/entity/PlayerEntity.java
@@ -61,8 +61,6 @@ public class PlayerEntity extends LivingEntity {
     private long lastSkinUpdate = -1;
     private boolean playerList = true;  // Player is in the player list
     private final EntityEffectCache effectCache;
-
-    private SkinProvider.SkinGeometry geometry;
 	
 	private SkinProvider.SkinGeometry geometry;
 
