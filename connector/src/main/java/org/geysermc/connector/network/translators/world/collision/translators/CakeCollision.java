9e3002d10897cccc8672a22760f97e50501326ea
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2020-09-09 17:19:33 +1000

    Update CakeCollision.java

--- connector/src/main/java/org/geysermc/connector/network/translators/world/collision/translators/CakeCollision.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/world/collision/translators/CakeCollision.java
@@ -34,7 +34,7 @@ public class CakeCollision extends BlockCollision {
     public CakeCollision(String params) {
         super();
         boundingBoxes = new BoundingBox[]{
-                new BoundingBox(0.5, 0.125, 0.5, 1, 0.25, 1)
+                new BoundingBox(0.5, 0.25, 0.5, 1, 0.5, 1)
         };
     }
 }
