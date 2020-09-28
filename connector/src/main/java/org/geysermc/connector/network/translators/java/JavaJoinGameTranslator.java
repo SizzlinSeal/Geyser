a12505e2a956525da7f79ece5b135da66c35d415
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2020-09-29 00:36:23 +1000

    Update JavaJoinGameTranslator.java

--- connector/src/main/java/org/geysermc/connector/network/translators/java/JavaJoinGameTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/java/JavaJoinGameTranslator.java
@@ -77,12 +77,12 @@ public class JavaJoinGameTranslator extends PacketTranslator<ServerJoinGamePacke
         gamerulePacket.getGameRules().add(new GameRuleData<>("doimmediaterespawn", !packet.isEnableRespawnScreen()));
         session.sendUpstreamPacket(gamerulePacket);
 
-        session.setServerRenderDistance(packet.getViewDistance());
+        session.setRenderDistance(packet.getViewDistance());
 
         // We need to send our skin parts to the server otherwise java sees us with no hat, jacket etc
         String locale = session.getClientData().getLanguageCode();
         List<SkinPart> skinParts = Arrays.asList(SkinPart.values());
-        ClientSettingsPacket clientSettingsPacket = new ClientSettingsPacket(locale, (byte) session.getClientRenderDistance(), ChatVisibility.FULL, true, skinParts, HandPreference.RIGHT_HAND);
+        ClientSettingsPacket clientSettingsPacket = new ClientSettingsPacket(locale, (byte) session.getRenderDistance(), ChatVisibility.FULL, true, skinParts, HandPreference.RIGHT_HAND);
         session.sendDownstreamPacket(clientSettingsPacket);
 
         session.sendDownstreamPacket(new ClientPluginMessagePacket("minecraft:brand", PluginMessageUtils.getGeyserBrandData()));
