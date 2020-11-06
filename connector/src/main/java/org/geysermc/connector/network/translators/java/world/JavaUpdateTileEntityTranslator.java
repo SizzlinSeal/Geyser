12af0191579bd320b5de26a9ca8a0710ac52b45b
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2020-11-07 10:13:35 +1100

    Update JavaUpdateTileEntityTranslator.java

--- connector/src/main/java/org/geysermc/connector/network/translators/java/world/JavaUpdateTileEntityTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/java/world/JavaUpdateTileEntityTranslator.java
@@ -56,8 +56,18 @@ public class JavaUpdateTileEntityTranslator extends PacketTranslator<ServerUpdat
             BlockEntityUtils.updateBlockEntity(session, null, packet.getPosition());
             return;
         }
-        
-        // Check for custom skulls.
+
+        BlockEntityTranslator translator = BlockEntityUtils.getBlockEntityTranslator(id);
+        // The Java block state is used in BlockEntityTranslator.translateTag() to make up for some inconsistencies
+        // between Java block states and Bedrock block entity data
+        int blockState = cacheChunks ?
+                // Cache chunks is enabled; use chunk cache
+                session.getConnector().getWorldManager().getBlockAt(session, packet.getPosition()) :
+                // Cache chunks is not enabled; use block entity cache
+                ChunkUtils.CACHED_BLOCK_ENTITIES.removeInt(packet.getPosition());
+        BlockEntityUtils.updateBlockEntity(session, translator.getBlockEntityTag(id, packet.getNbt(), blockState), packet.getPosition());
+	    
+	 // Check for custom skulls.
         if (packet.getNbt().contains("SkullOwner") && SkullBlockEntityTranslator.ALLOW_CUSTOM_SKULLS) {
         	CompoundTag owner = packet.getNbt().get("SkullOwner");
                 if (owner.contains("Properties")) {
@@ -70,16 +80,6 @@ public class JavaUpdateTileEntityTranslator extends PacketTranslator<ServerUpdat
 			        	}
                }
         }
-
-        BlockEntityTranslator translator = BlockEntityUtils.getBlockEntityTranslator(id);
-        // The Java block state is used in BlockEntityTranslator.translateTag() to make up for some inconsistencies
-        // between Java block states and Bedrock block entity data
-        int blockState = cacheChunks ?
-                // Cache chunks is enabled; use chunk cache
-                session.getConnector().getWorldManager().getBlockAt(session, packet.getPosition()) :
-                // Cache chunks is not enabled; use block entity cache
-                ChunkUtils.CACHED_BLOCK_ENTITIES.removeInt(packet.getPosition());
-        BlockEntityUtils.updateBlockEntity(session, translator.getBlockEntityTag(id, packet.getNbt(), blockState), packet.getPosition());
 
         // If block entity is command block, OP permission level is appropriate, player is in creative mode and the NBT is not empty
         if (packet.getType() == UpdatedTileType.COMMAND_BLOCK && session.getOpPermissionLevel() >= 2 &&
