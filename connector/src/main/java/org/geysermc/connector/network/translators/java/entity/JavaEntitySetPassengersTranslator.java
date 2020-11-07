e6f9d75e7a73e3fc9b943251063820a021bef2c4
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2020-11-08 10:00:07 +1100

    Update JavaEntitySetPassengersTranslator.java

--- connector/src/main/java/org/geysermc/connector/network/translators/java/entity/JavaEntitySetPassengersTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/java/entity/JavaEntitySetPassengersTranslator.java
@@ -153,7 +153,7 @@ public class JavaEntitySetPassengersTranslator extends PacketTranslator<ServerEn
                 mountedHeightOffset = 0;
                 break;
             case BOAT:
-                mountedHeightOffset = -0.25f;
+                mountedHeightOffset = -0.1f;
                 break;
             case HOGLIN:
             case ZOGLIN:
