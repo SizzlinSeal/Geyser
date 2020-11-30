eb667f601a06307da2ad828d99d3a958ae091854
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2020-11-30 18:54:07 +1100

    Update JavaPlayerListEntryTranslator.java

--- connector/src/main/java/org/geysermc/connector/network/translators/java/entity/player/JavaPlayerListEntryTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/java/entity/player/JavaPlayerListEntryTranslator.java
@@ -31,7 +31,7 @@ import org.geysermc.connector.network.session.GeyserSession;
 import org.geysermc.connector.network.translators.PacketTranslator;
 import org.geysermc.connector.network.translators.Translator;
 import org.geysermc.connector.utils.MessageUtils;
-import org.geysermc.connector.utils.SkinUtils;
+import org.geysermc.connector.skin.SkinManager;
 
 import com.github.steveice10.mc.protocol.data.game.PlayerListEntry;
 import com.github.steveice10.mc.protocol.data.game.PlayerListEntryAction;
