87a057286d8acff1f772998f488c4c890bc0715b
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2020-11-30 19:14:00 +1100

    Update SkinManager.java

--- connector/src/main/java/org/geysermc/connector/skin/SkinManager.java
+++ connector/src/main/java/org/geysermc/connector/skin/SkinManager.java
@@ -170,7 +170,6 @@ public class SkinManager {
                             PlayerListPacket.Entry updatedEntry = buildEntryManually(
                                     session,
                                     entity.getUuid(),
-                                    entity.getName(),
                                     entity.getGeyserId(),
                                     skin.getTextureUrl(),
                                     skin.getSkinData(),
